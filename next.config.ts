e73a47d9fac6d982a4e611abe3577e787099e0b3
callmedukey <87065409+callmedukey@users.noreply.github.com>
2025-06-11 11:40:49 +0900

    headers control

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,76 @@ const nextConfig: NextConfig = {
 
     return [
       {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
+        source: "/mattress-sofa",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
+        source: "/air-conditioner",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
+        source: "/entry-cleaning",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
+        source: "/apply-service",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
+        source: "/education-inquiry",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "public, max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
+        source: "/((?!api$|api/).*)",
+        headers: [
+          {
+            key: "Surrogate-Control",
+            value:
+              "max-age=600, stale-while-revalidate=14400, stale-if-error=14400",
+          },
+        ],
+      },
+      {
         source: "/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2|avif|webp)",
         locale: false,
         headers: [
