8434b6013d34e6f870e7a217ac24a87f8d8a80fc
callmedukey <87065409+callmedukey@users.noreply.github.com>
2025-06-11 11:18:44 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,24 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "50mb",
     },
   },
+  async headers() {
+    if (process.env.NODE_ENV !== "production") {
+      return [];
+    }
+
+    return [
+      {
+        source: "/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2|avif|webp)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
